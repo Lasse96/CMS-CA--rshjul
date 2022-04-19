812742c3a45c1d00bcfd05103f327ace6c052d6e
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-04-19 12:40:40 +0200

    js

---
+++ scripts/calendar.js
@@ -0,0 +1,28 @@
+const output = document.querySelector(".test");
+const url = "http://lassestrand.no/wp-json/tribe/events/v1/events";
+
+fetch(url)
+ .then (respons => respons.json())
+ .then (events => listEvents(events))
+ .catch((error) => {
+     console.error('Error:', error);
+});
+
+function listEvents (events) {
+    console.log(events.events);
+}
+
+// function listEvents (events) {
+//     let myList = "";
+//     for (let event of events) {
+//         console.log(event);
+//         myList += `
+//             <a href="product.html?id=${event.id}">
+//                 ${event.name}
+//             </a>`;
+//         console.log(event.name);
+//     }
+//     console.log(myList);
+//     output.innerHTML = myList;
+// }
+
