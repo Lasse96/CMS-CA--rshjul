931a0c757c5de97ced842b41240392754ba23989
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-04-21 15:42:03 +0200

    Linket opp

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -31,11 +31,20 @@ function listEvents (events) {
 
 // Filter -------------------------------------------------------------
 
-var filterbutton = document.querySelector(".filter-button");
+const filterbutton = document.querySelector(".filter-button");
+const showcont = document.querySelector(".filter-container");
+const showmnd = document.querySelector(".filter-måned");
+const showStud = document.querySelector(".student-select");
+const showAns = document.querySelector(".ansatt-select");
+const showStudies = document.querySelector(".studie-select");
+const showRoller = document.querySelector(".rolle-select");
+const showSkole = document.querySelector(".studiested-select");
+const showTyp = document.querySelector(".type-select");
+const showKvalitet = document.querySelector(".kvalitetsarbeid-select");
+
 
 filterbutton.addEventListener("click",
 function showcontainer() {
-   var showcont = document.querySelector(".filter-container");
    if (showcont.style.display === "none") {
        showcont.style.display = "block";
    } else {
@@ -45,7 +54,6 @@ function showcontainer() {
 );
 
 function showmaned() {
-    var showmnd = document.querySelector(".filter-måned");
     if (showmnd.style.display === "none") {
         showmnd.style.display = "block";
         // console.log(showmaned);
@@ -54,8 +62,30 @@ function showmaned() {
     }
 };
 
+function backToMenu() {
+    if (showmnd.style.display === "block") {
+        showmnd.style.display = "none"
+    } 
+    if (showStudies.style.display === "block") {
+        showStudies.style.display = "none"
+    }
+    if (showRoller.style.display === "block") {
+        showRoller.style.display = "none"
+    }
+    if (showSkole.style.display === "block") {
+        showSkole.style.display = "none"
+    } 
+    if (showKvalitet.style.display === "block") {
+        showKvalitet.style.display = "none"
+    } 
+    if (showTyp.style.display === "block") {
+        showTyp.style.display = "none"
+    } else {
+        showmnd.style.display = "none";
+    }
+}
+
 function showAnsatt() {
-    var showAns = document.querySelector(".ansatt-select");
     if (showAns.style.display === "none") {
         showAns.style.display = "block";
     } else {
@@ -64,8 +94,6 @@ function showAnsatt() {
 }
 
 function showStudent() {
-    var showAns = document.querySelector(".ansatt-select")
-    var showStud = document.querySelector(".student-select");
     if (showStud.style.display === "none") {
         showStud.style.display = "block";
         showAns.style.display = "none"    
@@ -73,3 +101,86 @@ function showStudent() {
         showStud.style.display = "none"
     }
 }
+
+function showStudie() {
+    if (showStudies.style.display === "none") {
+        showStudies.style.display = "block";
+    } else {
+        showStudies.style.display = "none";
+    }
+}
+
+function showRolle() {
+    if (showRoller.style.display === "none") {
+        showRoller.style.display = "block";
+    } else {
+        showRoller.style.display = "none";
+    }
+}
+
+function showStudiested() {
+    if (showSkole.style.display === "none") {
+        showSkole.style.display = "block";
+    } else {
+        showSkole.style.display = "none";
+    }
+}
+
+function showType() {
+    if (showTyp.style.display === "none") {
+        showTyp.style.display = "block";
+    } else {
+        showTyp.style.display = "none";
+    }
+}
+
+function showKvalArb() {
+    if (showKvalitet.style.display === "none") {
+        showKvalitet.style.display = "block";
+    } else {
+        showKvalitet.style.display = "none";
+    }
+}
+
+// var filterbutton = document.querySelector(".filter-button");
+
+// filterbutton.addEventListener("click",
+// function showcontainer() {
+//    var showcont = document.querySelector(".filter-container");
+//    if (showcont.style.display === "none") {
+//        showcont.style.display = "block";
+//    } else {
+//        showcont.style.display = "none";
+//    }
+// }
+// );
+
+// function showmaned() {
+//     var showmnd = document.querySelector(".filter-måned");
+//     if (showmnd.style.display === "none") {
+//         showmnd.style.display = "block";
+//         // console.log(showmaned);
+//     } else {
+//         showmnd.style.display = "none";
+//     }
+// };
+
+// function showAnsatt() {
+//     var showAns = document.querySelector(".ansatt-select");
+//     if (showAns.style.display === "none") {
+//         showAns.style.display = "block";
+//     } else {
+//         showAns.style.display = "none";
+//     }
+// }
+
+// function showStudent() {
+//     var showAns = document.querySelector(".ansatt-select")
+//     var showStud = document.querySelector(".student-select");
+//     if (showStud.style.display === "none") {
+//         showStud.style.display = "block";
+//         showAns.style.display = "none"    
+//     } else {
+//         showStud.style.display = "none"
+//     }
+// }
